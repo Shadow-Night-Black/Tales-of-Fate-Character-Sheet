a6e9eaae242b5f77eeddbc0a586225583340465c
Mark <m@aqu.me.uk>
2016-12-06 23:34:40 +0000

    Stuff

---
+++ data/Blessing.java
@@ -0,0 +1,7 @@
+package data;
+
+/**
+ * Created by shado on 06/12/2016.
+ */
+public class Blessing {
+}
---
+++ data/Totem.java
@@ -0,0 +1,10 @@
+package data;
+
+import java.util.List;
+
+/**
+ * Created by shado on 06/12/2016.
+ */
+public class Totem {
+  private List<Blessing> blessings;
+}
